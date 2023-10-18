f922a7962701207fe5f239234f0125e2a2612918
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-18 14:52:47 +0000

    finished refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to movies_url, :notice => "Movie created successfully."
     else
       render template: "movies/new"
     end
@@ -56,16 +56,16 @@ class MoviesController < ApplicationController
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where({ id: the_id }).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie),  notice: "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully." 
     end
   end
 
@@ -75,6 +75,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url,  notice: "Movie deleted successfully."
   end
 end
