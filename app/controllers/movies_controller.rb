08947d6b7e0436b73934b33078bda58acae04c00
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-19 16:17:50 +0000

    working on update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,54 +1,49 @@
 class MoviesController < ApplicationController
   def new
     @movie = Movie.new
-
   end
 
   def index
     @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
-      format.json do
-        render json: @list_of_movies
-      end
+      format.json { render json: @movies }
+
       format.html
     end
   end
 
   def show
     @movie = Movie.find(params.fetch(:id))
-
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_params = params.require(:movie).permit(:title, :description)
+    
+    @movie = Movie.new(movie_params)
 
     if @movie.valid?
       @movie.save
+
       redirect_to movies_url, notice: "Movie created successfully."
     else
-       "new"
+      render "new"
     end
   end
 
   def edit
     @movie = Movie.find(params.fetch(:id))
-
   end
 
   def update
     @movie = Movie.find(params.fetch(:id))
-
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
-
-    if @movie.valid?
-      @movie.save
-      redirect_to movie_url(@movie),  notice: "Movie updated successfully."
+                   
+    movie_params = params.require(:movie).permit(:title, :description)
+    
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie updated successfully."
     else
-      redirect_to movie_url(@movie), alert: "Movie failed to update successfully." 
+      render "edit"
     end
   end
 
@@ -57,6 +52,6 @@ class MoviesController < ApplicationController
 
     @movie.destroy
 
-    redirect_to movies_url,  notice: "Movie deleted successfully."
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
